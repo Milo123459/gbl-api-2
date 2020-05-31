6d51e467b4d63b6b3aaf034f546786f879bf62eb
Milo123459 <milo5huntington@gmail.com>
2020-05-31 06:03:35 +0100

    First update

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.use("/static", express.static("images"));
+app.set("view engine", "ejs");
+app.get("/", async (req, res) => res.render("index"));
+app.listen(3000, () => console.log("Listening"));
