cd6dd36947a91d12ece965cfeaa4b47a2e9e1c27
Milo123459 <milo5huntington@gmail.com>
2020-06-07 15:47:38 +0100

    GBL Website is a go

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const express = require("express");
 const app = express();
 app.use("/static", express.static("images"));
 app.set("view engine", "ejs");
-app.get("/", async (req, res) => res.render("index"));
+app.get("/", async (req, res) =>
+  res.render("index", {
+  })
+);
+app.use(function(req,res){
+  res.status(404);
+  res.render("404",{
+    URL: req.url
+  })
+})
 app.listen(3000, () => console.log("Listening"));
